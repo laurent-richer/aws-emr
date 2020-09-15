8f9f0033842fce36ae38bd6282226eba01017554
root <root@ip-10-0-0-222.eu-west-3.compute.internal>
2020-09-15 10:23:07 +0000

    First release of script

--- emr_pingbox.sh
+++ emr_pingbox.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-set -x
+#set -x
+# Set the ip address you would lik to ping from the master of your EMR cluster
 IP_ADDRESS=8.8.8.8
+#Set the region where you would like the metrics to be published ( usually same as your EMR Cluster)
 REGION=eu-west-3
-FILE=/tmp/ping_box.sh
+#PING frequency in seconds
+FREQ=1
+
+FILE=/tmp/emr_pingbox.sh
 INSTANCE_ID=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
 
-while true;
-do
-  AVG_PING=`ping -c 1 $IP_ADDRESS  | awk -F\/ '/rtt/ {print $5}'`
-  aws cloudwatch put-metric-data --metric-name ping --namespace EMR --unit Milliseconds --value "$AVG_PING" --dimensions InstanceId=$INSTANCE_ID --storage-resolution 1 --region $REGION
-  echo "Host is online : $AVG_PING"
-  sleep 1
-done
+echo "#!/bin/bash">$FILE
+echo "while true;">>$FILE
+echo "do">>$FILE
+echo "  AVG_PING=\`ping -c 1 $IP_ADDRESS  | awk -F\/ '/rtt/ {print \$5}'\`">>$FILE
+echo "  aws cloudwatch put-metric-data --metric-name ping --namespace EMR --unit Milliseconds --value \"\$AVG_PING\" --dimensions InstanceId=$INSTANCE_ID --storage-resolution 1 --region $REGION">>$FILE
+echo "  sleep $FREQ">>$FILE
+echo "done">>$FILE
+
+chmod +x $FILE
+#launch in background
+nohup $FILE 1>/dev/null 2>/dev/null &
